0ecb39042d944e8c27668bed135be59d2a5ae331
Murad Rogozhnikov <murad@rogozhnikoff.com>
2015-11-05 22:57:31 +0200

    youchart in react

--- js/component/youchart.jsx
+++ js/component/youchart.jsx
@@ -1,34 +1,65 @@
 const React = require('react');
+const cx = require('classnames');
 
 class Youchart extends React.Component {
   render() {
     const {
         count,
         name,
-        level
-        } = this.props
+        level,
+        type,
+        } = this.props;
+
+    const titleClass = cx({
+      'youchart-title': true,
+      ['youchart-title-lvl_' + level]: true
+    });
+
+    const lineClass = cx({
+      'youchart-line': true,
+      'youchart-line-step': type === 'step',
+      'youchart-line-grad': type === 'grad'
+    })
+
+    const stepsClass = cx({
+      'youchart-steps': true,
+      'youchart-steps-grad': type === 'grad'
+    })
+
+
+    const items = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(function(c){
+      const num = (type === 'grad' && c !== 0)
+          ? c + '0'
+          : c;
+
+      const classPercent = 'youchart-step-percent_' + num // почему это валится с ошибкой компиляции ?
+
+      const itemClass = cx({
+        "youchart-step": true,
+        ["youchart-step-lvl_" + c]: type === 'step',
+        [classPercent]: type === 'grad' && (c === 0 || c === 10),
+        "youchart-step-grad": type === 'grad'
+      });
+
+      return (<li className={itemClass} key={c}>
+        {num}
+      </li>)
+    });
+
     return (<div className="youchart">
-      <div className={'youchart-title youchart-title-lvl_' + level}>
-        <div className="youchart-name">{name}</div>
-        <div className="youchart-count">{count}</div>
+      <div className={titleClass}>
+        <div className="youchart-inside">
+          <div className="youchart-name">{name}</div>
+          <div className="youchart-count">{count}</div>
+        </div>
       </div>
-      <div className={'youchart-line youchart-line-steps youchart-line-lvl_' + level}>
-        <ul className="youchart-steps">
-          <li className="youchart-step youchart-step-lvl_0">0</li>
-          <li className="youchart-step youchart-step-lvl_1">1</li>
-          <li className="youchart-step youchart-step-lvl_2">2</li>
-          <li className="youchart-step youchart-step-lvl_3">3</li>
-          <li className="youchart-step youchart-step-lvl_4">4</li>
-          <li className="youchart-step youchart-step-lvl_5">5</li>
-          <li className="youchart-step youchart-step-lvl_6">6</li>
-          <li className="youchart-step youchart-step-lvl_7">7</li>
-          <li className="youchart-step youchart-step-lvl_8">8</li>
-          <li className="youchart-step youchart-step-lvl_9">9</li>
-          <li className="youchart-step youchart-step-lvl_10">10</li>
+      <div className={lineClass}>
+        <ul className={stepsClass}>
+          {items}
         </ul>
       </div>
       {this.props.children}
-    </div>)
+    </div>);
   }
 }
 
@@ -37,6 +68,17 @@ Youchart.defaultProps = {
   count: 0,
   name: 'Noname',
   level: 1,
+  type: 'step',
 };
 
-module.exports = Youchart
+Youchart.propTypes = {
+  level: function(props, propName, componentName) {
+    if (!(props[propName] >= 0 && props[propName] <= 10)) {
+      return new Error('level ' + props[propName] + ' is not allowed');
+    } else {
+      return null
+    }
+  }
+};
+
+module.exports = Youchart;

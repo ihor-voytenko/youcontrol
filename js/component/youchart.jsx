d28f9a465be7a5b2bf8b703d1a3c494bc716fc0e
Murad Rogozhnikov <murad@rogozhnikoff.com>
2015-11-09 03:58:47 +0200

    youchart

--- js/component/youchart.jsx
+++ js/component/youchart.jsx
@@ -132,7 +132,7 @@ class Youchart extends React.Component {
             </div>
           </div>
         </div>
-      } else if (type === 'grad' && type === 'step') {
+      } else if (type === 'grad' || type === 'step') {
 
         const items = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(function (c) {
           const num = (type === 'grad' && c !== 0)

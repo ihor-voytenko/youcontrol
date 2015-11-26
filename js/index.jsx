2b5aa5e3004f2a330649f7b611d6608452bea52f
Igor Voytenko <skyivoyt@gmail.com>
2015-11-26 16:25:21 +0200

    polyfil

--- js/index.jsx
+++ js/index.jsx
@@ -1,5 +1,9 @@
 'use strict'
 
+require('./vendor/es5-shim.js');
+require('./vendor/es5-sham.js');
+require('./vendor/console.pol.js');
+
 var React = require('react');
 
 require('./jquery-to-react').init({
